0157b52f5a88e8bd779a65ca0d6ba00c8e64f763
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-09 21:05:53 +0100

    update Data.tsx

--- src/Data.tsx
+++ src/Data.tsx
@@ -1,4 +1,32 @@
-export const project(){
-
-    
-}
+export const project = [
+  {
+    id: 1,
+    img: "./assets/Advice.png",
+    title: "Advice Box",
+    lang: "React Js & Tailwind Css",
+  },
+  {
+    id: 2,
+    img: "./assets/SwyftPay.png",
+    title: "Swyft Pay",
+    lang: "React Js & Tailwind Css",
+  },
+  {
+    id: 3,
+    img: "./assets/KryptoSkool.png",
+    title: "Krypto Skool",
+    lang: "React Js & Tailwind Css",
+  },
+  {
+    id: 4,
+    img: "./assets/unity.png",
+    title: "Unity Project",
+    lang: "React Js & Tailwind Css",
+  },
+  {
+    id: 5,
+    img: "./assets/unity.png",
+    title: "Another Unity Project",
+    lang: "React Js & Tailwind Css",
+  },
+];

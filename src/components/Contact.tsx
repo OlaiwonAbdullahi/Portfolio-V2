fd158cc557f8b7718b64558bffc026d7c02129a6
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-09 19:10:37 +0100

    update Contact.tsx and Skills.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -27,20 +27,20 @@ export default function Contact() {
             type="text"
             placeholder="Your Name"
             id="name"
-            className="w-full md:w-3/4 lg:w-1/2 px-3 py-2 border border-Reserved rounded-md bg-Background mb-4 text-sm md:text-base"
+            className="w-full md:w-3/4 lg:w-1/2 px-3 py-2 border border-Reserved rounded-md bg-Background mb-4 text-sm md:text-base text-NavColor font-inter"
           />
 
           <input
             type="email"
             placeholder="Your Email"
             id="email"
-            className="w-full md:w-3/4 lg:w-1/2 px-3 py-2 border border-Reserved rounded-md bg-Background mb-4 text-sm md:text-base"
+            className="w-full md:w-3/4 lg:w-1/2 px-3 py-2 border border-Reserved rounded-md bg-Background mb-4 text-sm md:text-base text-NavColor font-inter"
           />
 
           <textarea
             placeholder="Your Message"
             id="message"
-            className="w-full md:w-3/4 lg:w-1/2 h-36 px-3 py-2 border border-Reserved rounded-md bg-Background text-sm md:text-base"
+            className="w-full md:w-3/4 lg:w-1/2 h-36 px-3 py-2 border border-Reserved rounded-md bg-Background text-sm md:text-base text-NavColor font-inter"
           />
         </form>
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,27 +7,55 @@ import react from "../assets/react.svg";
 import github from "../assets/github.svg";
 import bootsrap from "../assets/bootsrap.svg";
 import tailwind from "../assets/tailwind-css.svg";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+
 export default function Skills() {
   return (
-    <div className="text-center px-4 md:px-0 ">
-      <h2 className="text-2xl md:text-4xl text-white font-inter font-semibold mt-24">
+    <div className="text-center px-4 md:px-0">
+      <h2
+        className="text-2xl md:text-4xl text-white font-inter font-semibold mt-24"
+        data-aos="fade-right"
+      >
         My Tech Skills
       </h2>
-      <span className="bg-gradient-to-r from-textColor to-white bg-clip-text text-transparent font-poppins mt-1 text-base md:text-lg">
+      <span
+        className="bg-gradient-to-r from-textColor to-white bg-clip-text text-transparent font-poppins mt-1 text-base md:text-lg"
+        data-aos="fade-left"
+      >
         Explore My Programming Skills
       </span>
 
-      <div className="flex flex-col md:flex-row items-center gap-6 mx-auto justify-center mt-14">
-        <img src={html} alt="" className="h-20 w-20" />
-        <img src={css} alt="" className="h-20 w-20" />
-        <img src={js} alt="" className="h-20 w-20" />
-        <img src={sass} alt="" className="h-20 w-20" />
-        <img src={bootsrap} alt="" className="h-20 w-20" />
-        <img src={react} alt="" className="h-20 w-20" />
-        <img src={git} alt="" className="h-20 w-20" />
-        <img src={github} alt="" className="h-20 w-20" />
-        <img src={tailwind} alt="" className="h-20 w-20" />
-        <img src={git} alt="" className="h-20 w-20" />
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6 mx-auto justify-center mt-14">
+        <div className="flex justify-center items-center" data-aos="fade-right">
+          <img src={html} alt="HTML" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-left">
+          <img src={css} alt="CSS" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-right">
+          <img src={js} alt="JavaScript" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-left">
+          <img src={sass} alt="SASS" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-right">
+          <img src={bootsrap} alt="Bootstrap" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-left">
+          <img src={react} alt="React" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-right">
+          <img src={git} alt="Git" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-left">
+          <img src={github} alt="GitHub" className="h-20 w-20" />
+        </div>
+        <div className="flex justify-center items-center" data-aos="fade-right">
+          <img src={tailwind} alt="Tailwind CSS" className="h-20 w-20" />
+        </div>
       </div>
     </div>
   );

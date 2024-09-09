e2bf901fc08b2706bdfeb215d137dea1140a6859
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-09-09 17:23:17 +0100

    update index.html, Hero.tsx and NavBar.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,8 @@
 import HeroImg from "../assets/HeroImg.png";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 export default function Hero() {
   return (
@@ -7,21 +11,34 @@ export default function Hero() {
         src={HeroImg}
         alt="Hero Image"
         className="h-40 w-40 md:h-56 md:w-56 rounded-full my-2"
+        data-aos="fade-right"
       />
-      <h1 className="text-3xl md:text-5xl text-white font-poppins my-4 font-semibold text-center">
+      <h1
+        className="text-3xl md:text-5xl text-white font-poppins my-4 font-semibold text-center"
+        data-aos="fade-left"
+      >
         Abdullahi Olaiwon
       </h1>
 
-      <span className="bg-gradient-to-r from-textColor to-white bg-clip-text text-transparent font-poppins mt-1 text-lg md:text-xl">
+      <span
+        className="bg-gradient-to-r from-textColor to-white bg-clip-text text-transparent font-poppins mt-1 text-lg md:text-xl"
+        data-aos="fade-right"
+      >
         FrontEnd Developer
       </span>
-      <p className="text-white text-sm md:text-base font-poppins text-center mx-4 md:mx-auto w-full md:w-2/3 mt-4">
+      <p
+        className="text-white text-sm md:text-base font-poppins text-center mx-4 md:mx-auto w-full md:w-2/3 mt-4"
+        data-aos="fade-up"
+      >
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum fuga
         excepturi enim placeat quas rerum fugiat, hic assumenda necessitatibus
         quae fugit accusantium autem voluptatum repellat quos. Repudiandae,
         repellat. Temporibus, harum!
       </p>
-      <button className="bg-transparent border-textColor rounded-3xl border px-6 py-2 mt-4 font-poppins text-white text-sm md:text-base">
+      <button
+        className="bg-transparent border-textColor rounded-3xl border px-6 py-2 mt-4 font-poppins text-white text-sm md:text-base"
+        data-aos="fade-up"
+      >
         Contact Me
       </button>
     </div>

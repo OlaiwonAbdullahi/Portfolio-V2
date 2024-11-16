e9f4e901274432f79617d9d0ff36f4c420687e4e
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-11-16 09:28:07 +0100

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -13,7 +13,7 @@ interface Project {
   lang: string;
   github: string;
   liveLink: string;
-  contributor: string;
+  contributor?: string;
 }
 
 interface WorkProps {
@@ -79,9 +79,9 @@ function Work({ work }: WorkProps) {
       <span className="text-textColor font-kanit mt-5 text-sm block">
         Technologies Used: {work.lang}
       </span>
-      <span className="text-Reserved italic text-sm">
-        Contributors: {work.contributor || "null"}
-      </span>
+     <span className="text-Reserved italic text-sm">
+  Contributors: {work.contributor || "N/A"}
+</span>
     </div>
   );
 }

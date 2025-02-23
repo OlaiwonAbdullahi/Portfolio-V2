ae4dcd29110e3112d9feade012c2ddeede036269
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2025-02-23 18:50:40 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content is available. Reload?")) {
+      updateSW();
+    }
+  }
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
